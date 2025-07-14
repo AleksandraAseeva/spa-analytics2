aa17a49370969123bc6e8b8370e29485297405a3
AleksandraAseeva <aleksandrplatc5@gmail.com>
2025-07-14 20:35:45 +0300

    Update OrdersView.vue

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -198,7 +198,7 @@ const fetchProducts = async () => {
   error.value = null;
   apiRequestError.value = false;
   try {
-    const url = new URL("http://109.73.206.144:6969/api/orders");
+    const url = new URL("/api/orders");
 
     Object.entries(apiFilters.value).forEach(([key, value]) => {
       if (value !== null && value !== undefined) {
